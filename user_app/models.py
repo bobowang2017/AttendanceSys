c3e1928f67a09c491e0ff99ae9535b355dda0243
xbwangh <xbwangh@isoftstone.com>
2017-06-30 21:36:56 +0800

    修改数据库同步的中文乱码问题

--- user_app/models.py
+++ user_app/models.py
@@ -10,25 +10,25 @@ class User(models.Model):
         (1, 'manager'),
     )
     user_status=(
-        (0, '正常状态'),
-        (-1, '禁用状态'),
-        (-2, '过期'),
+        (0, u'正常状态'),
+        (-1, u'禁用状态'),
+        (-2, u'过期'),
     )
-    user_id = models.CharField(max_length=64, verbose_name='序号', primary_key=True)
-    user_number = models.CharField(max_length=10, verbose_name='编号', unique=True)
-    user_name = models.CharField(max_length=64, verbose_name='姓名')
-    user_sex = models.BooleanField(default=True, verbose_name='性别')
-    user_birthday = models.DateTimeField(verbose_name='出生日期')
-    user_email = models.CharField(max_length=64, verbose_name='邮箱')
-    user_phone = models.CharField(max_length=11, verbose_name='手机号码')
-    user_address = models.CharField(max_length=256, verbose_name='家庭住址', default=0)
-    user_password = models.CharField(max_length=256, verbose_name='用户密码', null=True)
-    user_role = models.IntegerField(choices=role_type, verbose_name='用户角色类型', default=0)
-    register_time = models.DateTimeField(auto_now_add=True, verbose_name='注册时间')
-    user_status = models.IntegerField(choices=user_status, verbose_name='状态',  default=0)
+    user_id = models.CharField(max_length=64, verbose_name=u'序号', primary_key=True)
+    user_number = models.CharField(max_length=10, verbose_name=u'编号', unique=True)
+    user_name = models.CharField(max_length=64, verbose_name=u'姓名')
+    user_sex = models.BooleanField(default=True, verbose_name=u'性别')
+    user_birthday = models.DateTimeField(verbose_name=u'出生日期')
+    user_email = models.CharField(max_length=64, verbose_name=u'邮箱')
+    user_phone = models.CharField(max_length=11, verbose_name=u'手机号码')
+    user_address = models.CharField(max_length=256, verbose_name=u'家庭住址', default=0)
+    user_password = models.CharField(max_length=256, verbose_name=u'用户密码', null=True)
+    user_role = models.IntegerField(choices=role_type, verbose_name=u'用户角色类型', default=0)
+    register_time = models.DateTimeField(auto_now_add=True, verbose_name=u'注册时间')
+    user_status = models.IntegerField(choices=user_status, verbose_name=u'状态',  default=0)
 
     class Meta:
-        verbose_name = '用户表'
+        verbose_name = u'用户表'
         verbose_name_plural = verbose_name
         db_table = 'user'
 
